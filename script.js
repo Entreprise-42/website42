6d30bcc712dff1da2697d89610a2a621ad40f353
Entreprise-42 <entreprisefortytwo@gmail.com>
2025-01-17 10:32:29 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// pour récupérer le nom et prénom
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Empêche le rechargement de la page
+    alert('Merci pour votre message !');
+});
+
+
+
+
